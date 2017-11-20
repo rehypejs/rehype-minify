f208c83335c1265d251aaebe2f27a42a60820029
Andrew Joslin <andrewtjoslin@gmail.com>
2017-11-20 14:48:46 -0700

    rehype-javascript-to-bottom: add support for filtering
    Closes GH-21.

--- packages/rehype-javascript-to-bottom/index.js
+++ packages/rehype-javascript-to-bottom/index.js
@@ -16,41 +16,48 @@ var js = require('hast-util-is-javascript');
 
 module.exports = jsToBottom;
 
-function jsToBottom() {
+function jsToBottom(options) {
+  var settings = options || {};
+  var filter = settings.filter || yes;
+
   return transform;
+
+  function transform(tree) {
+    var matches = [];
+    var body;
+
+    visit(tree, 'element', visitor);
+
+    if (body && matches.length !== 0) {
+      move();
+    }
+
+    function visitor(node, index, parent) {
+      if (node.tagName === 'body') {
+        body = node;
+      }
+
+      if (js(node) && filter(node)) {
+        matches.push([parent, node]);
+      }
+    }
+
+    function move() {
+      var length = matches.length;
+      var index = -1;
+      var match;
+      var siblings;
+
+      while (++index < length) {
+        match = matches[index];
+        siblings = match[0].children;
+        siblings.splice(siblings.indexOf(match[1]), 1);
+        body.children.push(match[1]);
+      }
+    }
+  }
 }
 
-function transform(tree) {
-  var matches = [];
-  var body;
-
-  visit(tree, 'element', visitor);
-
-  if (body && matches.length !== 0) {
-    move();
-  }
-
-  function visitor(node, index, parent) {
-    if (node.tagName === 'body') {
-      body = node;
-    }
-
-    if (js(node)) {
-      matches.push([parent, node]);
-    }
-  }
-
-  function move() {
-    var length = matches.length;
-    var index = -1;
-    var match;
-    var siblings;
-
-    while (++index < length) {
-      match = matches[index];
-      siblings = match[0].children;
-      siblings.splice(siblings.indexOf(match[1]), 1);
-      body.children.push(match[1]);
-    }
-  }
+function yes() {
+  return true;
 }
