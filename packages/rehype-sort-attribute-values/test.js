73a5d12f3ef606222029211420e8126657cb55d7
Titus Wormer <tituswormer@gmail.com>
2018-11-17 15:56:40 +0100

    sort-attribute-values: add better sorting

--- packages/rehype-sort-attribute-values/test.js
+++ packages/rehype-sort-attribute-values/test.js
@@ -11,18 +11,51 @@ test('rehype-sort-attribute-values', function(t) {
   t.deepEqual(
     rehype()
       .use(min)
-      .runSync(
-        h('i', {
-          className: ['qux', 'quux', 'bar', 'foo', 'baz'],
-          itemProp: true,
-          id: 'foo'
-        })
-      ),
-    h('i', {
-      className: ['bar', 'baz', 'foo', 'quux', 'qux'],
-      itemProp: true,
-      id: 'foo'
-    })
+      .runSync(h('#foo.qux.quux.bar.foo.baz', {itemProp: true})),
+    h('#foo.bar.baz.foo.quux.qux', {itemProp: true})
+  )
+
+  // 3 x foo, 2 x bar, 1 x baz, 1 x qux, 1 x quux
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foo', [h('.bar.foo'), h('.quux'), h('.qux.baz.bar.foo')])),
+    h('.foo', [h('.foo.bar'), h('.quux'), h('.foo.bar.baz.qux')])
+  )
+
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foobar', [h('.foo.foobar')])),
+    h('.foobar', [h('.foobar.foo')])
+  )
+
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foo.foo')),
+    h('.foo.foo')
+  )
+
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foo.foob')),
+    h('.foo.foob')
+  )
+
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foob.foo')),
+    h('.foo.foob')
+  )
+
+  t.deepEqual(
+    rehype()
+      .use(min)
+      .runSync(h('.foob.fooa')),
+    h('.fooa.foob')
   )
 
   t.end()
